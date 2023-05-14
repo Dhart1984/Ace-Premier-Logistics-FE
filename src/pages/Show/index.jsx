2e95c64e0f7a87d4fef3e3e9b969e67f16b05314
Dhart1984 <126024205+Dhart1984@users.noreply.github.com>
2023-05-14 15:42:04 -0400

    Revert "Dev"

--- src/pages/Show/index.jsx
+++ src/pages/Show/index.jsx
@@ -1,104 +1,11 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router'
-import { getApplicant, deleteApplicant} from '../../utilities/applicant-service'
-import {  useNavigate } from 'react-router-dom'
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Button
-} from "@material-tailwind/react";
 
-const Show = () => {
-  const [applicant, setApplicant] = useState(null)
-  const { id } = useParams()
 
- const handleRequest = async () => {
-		try {
-				const applicantData = await getApplicant(id)
-				setApplicant(applicantData)
-		}catch(err){
-				console.log(err)
-		}
+function ShowPage(props){
+    return(
+        <div>
+            <h1>ShowPage</h1>
+        </div>
+    )
 }
 
-  console.log(`Current Applicant: ${JSON.stringify(applicant)}`)
-
-  useEffect(() => {
-    handleRequest()
-  }, [])
-
-  
-// After testing that person is defined you can use a ternary statement to
-// conditionally render either a person's data or a loading message
-
-const loaded = () => (
-  <Card className="mt-6 w-96">
-      <CardHeader color="blue-gray" className="relative h-56">
-        <img src="https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_1024,h_683/https://warehouse.ninja/wp-content/uploads/2021/11/amazon-delivery-van-1024x683.jpg" alt="img-blur-shadow" layout="fill" />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          {applicant.firstName}
-        </Typography>
-        <Typography>
-        {applicant.lastName}
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button className='delete' onClick={handleDelete}>
-        Delete Applicant</Button>
-      </CardFooter>
-    </Card>
-    
-)
-
-const loading = () => {
-    return <h1>Loading.........</h1>
-            // alternatively you can use the spinner
-}
-
-
-const navigate = useNavigate()
-
-const handleDelete = async () => {
-    
-        try {
-      
-      const deletedResp = await deleteApplicant(id)
-
-       console.log(deletedResp)
-
-      navigate('/')
-      // navigate will change the browser's URL
-      // which will cause react-router to "redirect" to home page;
-      // the Main will then re-render the People component
-      // upon mount People will fetch the updated index of people data
-
-  } catch (err) {
-      console.log(err)
-      navigate(`/people/${id}`)
-  }
-}
-
-
-
-
-
-
-
-
-return <div>
-  {applicant ? loaded() : loading()}
-</div>
-
-
-
-
-
-
-}
-
-export default Show
+export default ShowPage
--- src/utilities/applicant-service.js
+++ src/utilities/applicant-service.js
@@ -2,41 +2,13 @@ import * as applicantAPI from './applicant-api'
 // because the api will export an exports object all modules can be imported with an * (wildcard)
 // the as import keywork will provide a variable reference to the different sub-modules we will export from people-api.js 
 
-export async function getApplicants(){
-    try {
-        const data = await applicantAPI.index()
-        return data
-		}catch(err){
-        return err
-    }
-    
-}
 export async function createApplicant(data){
     try {
-        const applicantData = await applicantAPI.create(data)
-        return applicantData 
+        // const applicantData = await applicantAPI.create(data)
+        // return applicantData 
     } catch (err) {
         console.log(err)
         throw new Error(err)
     }
 }
-
-export async function getApplicant(id){
-    try {
-        const foundApplicant = await applicantAPI.detail(id)
-        return foundApplicant
-    } catch (err) {
-        console.log(err)
-        throw new Error(err)
-    }
-}
-
-export async function deleteApplicant(id){
-    try {
-        const deletedApplicant = await applicantAPI.destroy(id)
-        return deletedApplicant
-    }catch(err){
-        console.log(err)
-        throw new Error(err)
-    }
-}
+//is my appplicant api th ename of my be data? 
